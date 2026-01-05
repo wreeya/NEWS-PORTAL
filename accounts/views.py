81e766c03997447239228a56026cc3d760140909
Riya Bhattarai <kan078bct065@kec.edu.np>
2026-01-05 21:02:47 +0545

    Made register and login at the same time

--- accounts/views.py
+++ accounts/views.py
@@ -17,9 +17,10 @@ class RegisterView(View):
             user = form.save()
 
             # Optional: log the user in immediately after registration
-            # login(request, user)
+            login(request, user)
 
             # Redirect to login page after successful registration
-            return redirect("accounts:login")
+            #return redirect("accounts:login")
+            return redirect('home')
 
         return render(request, "registration/register.html", {"form": form})
