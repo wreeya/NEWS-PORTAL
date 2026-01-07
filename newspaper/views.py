3fffcd4456a4db0b1f019a7d547367a04e4ac66c
Riya Bhattarai <kan078bct065@kec.edu.np>
2026-01-07 19:47:14 +0545

    displaying message and used AJAX in Newsletter

--- newspaper/views.py
+++ newspaper/views.py
@@ -63,35 +63,6 @@ class PostListView(SidebarMixin,ListView):
             published_at__isnull=False, status='active'
         ).order_by("-published_at")
 
-class PostDetailView(SidebarMixin,DetailView):
-    model = Post
-    template_name = 'newsportal/detail/detail.html'
-    context_object_name = 'post'
-
-    def get_queryset(self):
-        query = super().get_queryset()
-        query = query.filter(published_at__isnull=False, status='active')
-        return query
-
-    def get_context_data(self,**kwargs):
-        context = super().get_context_data(**kwargs)
-        #increasing the views count of currently viewed post
-        current_post = self.object
-        current_post.views_count += 1
-        current_post.save()
-
-        context["related_posts"] = (
-            Post.objects.filter(
-                published_at__isnull=False,
-                status="active",
-                category=self.object.category,
-            )
-            .exclude(id=self.object.id)
-            .order_by("-published_at", "-views_count") [:2]
-        )
-        return context
-
-
 class AboutView(TemplateView):
     template_name = 'newsportal/about.html'
 
