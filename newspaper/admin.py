37498ea274fd69a3aa3d8473799627381b8d8bbe
Riya Bhattarai <kan078bct065@kec.edu.np>
2026-01-12 21:16:05 +0545

    created a new post and used third party library to edit content

--- newspaper/admin.py
+++ newspaper/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 from .models import Advertisement, Category, Post, Tag, OurTeam, Contact, Comment, Newsletter
-
+from django_summernote.admin import SummernoteModelAdmin
 # Register your models here.
 
-admin.site.register(Post)
+# admin.site.register(Post)
 admin.site.register(Tag)
 admin.site.register(Category)
 admin.site.register(Advertisement)
@@ -11,3 +11,7 @@ admin.site.register(OurTeam)
 admin.site.register(Contact)
 admin.site.register(Comment)
 admin.site.register(Newsletter)
+
+class PostAdmin(SummernoteModelAdmin):
+    summernote_fields = ('content',)
+admin.site.register(Post, PostAdmin)
