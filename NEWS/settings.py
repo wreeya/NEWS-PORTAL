4fc8410ee932619ec33250612ca0efc78d9ba5cc
Riya Bhattarai <kan078bct065@kec.edu.np>
2026-01-12 21:54:33 +0545

    Updated admin interface using third party library ie django-unfold

--- NEWS/settings.py
+++ NEWS/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "unfold",  # before django.contrib.admin
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
